d67d4efd4abd3194f1067a30839acda9f537a91d
AmirSTM <amirstm@gmail.com>
2023-09-18 10:39:11 +0330

    Code Order class in trade module

--- tse_utils/models/enums.py
+++ tse_utils/models/enums.py
@@ -9,3 +9,29 @@ class Nsc(Enum):
     IG = "ممنوع مسدود"
     IR = "ممنوع محفوظ"
     IS = "ممنوع متوقف"
+
+class TradeSide(Enum):
+    BUY = "خرید"
+    SELL = "فروش"
+
+class OrderState(Enum):
+    SENT_TO_CORE = "ارسال به هسته"
+    ACTIVE = "فعال"
+    EXECUTED = "اجرا شده"
+    CANCELED = "حذف شده"
+    ERROR = "خطا"
+
+    def is_active(self):
+        return self == OrderState.ACTIVE
+    
+class OrderValidity(Enum):
+    DAY = "روز"
+    GOOD_TILL_CANCEL = "معتبر تا لغو"
+    GOOD_TILL_DATE = "معتبر تا تاریخ"
+    FILL_OR_KILL = "اجرا و حذف"
+
+class OrderLock(Enum):
+    UNLOCK = "آزاد"
+    LOCK_FOR_CREATION = "قفل برای ایجاد"
+    LOCK_FOR_EDITION = "قفل برای ویرایش"
+    LOCK_FOR_CANCELATION = "قفل برای حذف"
--- tse_utils/models/trade.py
+++ tse_utils/models/trade.py
@@ -1,3 +1,50 @@
 from dataclasses import dataclass
-from tse_utils.models.realtime import *
+from tse_utils.models.enums import *
 from datetime import time, date, datetime
+
+@dataclass
+class TraderIdentification:
+    """
+    Holds the identification for a trader.
+    """
+    id: int = None
+    username: str = None
+    password: str = None
+    first_name: str = None
+    last_name: str = None
+
+    def __str__(self):
+        return f"{self.username}"
+
+class Order:
+    """
+    Holds data for a single order from a trader.
+    """
+    def __init__(self, id, isin: str, side: TradeSide, quantity: int = None, price: int = None,
+                 remaining_quantity: int = None, executed_quantity: int = None, state: OrderState = None,
+                 client_side_id: str = None, hon: str = None, blocked_credit: int = None, creation_datetime: datetime = None,
+                 validity: OrderValidity = None, expiration_date: date = None, lock: OrderLock = None):
+        self.id = id
+        self.isin = isin
+        self.side = side
+        self.quantity = quantity
+        self.price = price
+        self.remaining_quantity = remaining_quantity
+        self.executed_quantity = executed_quantity
+        self.state = state
+        """
+        client_side_id is used for mapping the order on the OMS side with the one sent by a client.
+        """
+        self.client_side_id = client_side_id
+        self.hon = hon
+        self.blocked_credit = blocked_credit
+        self.creation_datetime = creation_datetime
+        self.validity = validity
+        """
+        expiration_date is used for orders validity of which are GOOD_TILL_DATE
+        """
+        self.expiration_date = expiration_date
+        self.lock = lock
+
+    def __str__(self):
+        return f"{self.side}|{self.state}|{self.id}|{self.isin}|P:{self.price},Q:{self.quantity}"
