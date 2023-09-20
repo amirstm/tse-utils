a89cc34c0e2b667e8069dd85b4e799cf8a3b6729
AmirSTM <amirstm@gmail.com>
2023-09-20 14:34:58 +0330

    Add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="tse_utils",
+    version="0.0.2",
+    author="Arka Equities & Securities",
+    author_email="info@arkaequities.com",
+    description="General utilities used for data mining in Tehran Stock Exchange (TSE).",
+    packages=setuptools.find_packages(),
+    install_requires=["httpx", "beautifulsoup4"],
+    classifiers= [
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
