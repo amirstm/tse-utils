327cfea46f92c6fee703eb1d9a565947561f45d4
AmirSTM <amirstm@gmail.com>
2023-09-21 18:12:49 +0330

    Add pypi publish to github actions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="tse_utils",
-    version="0.0.2",
+    version="0.0.3",
     author="Arka Equities & Securities",
     author_email="info@arkaequities.com",
     description="General utilities used for data mining in Tehran Stock Exchange (TSE).",
