0530f37490c88dafec6676b2a64c97e8037af476
AmirSTM <amirstm@gmail.com>
2023-09-23 13:37:35 +0330

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setuptools.setup(
     author="Arka Equities & Securities",
     author_email="info@arkaequities.com",
     description="General utilities used for data mining in Tehran Stock Exchange (TSE).",
+    long_description="Utilities for data mining and auto-trading in Tehran Stock Exhchange. \
+        Contains TSETMC API implementation and other interfaces useful for securities market \
+        data catching and processing",
     packages=setuptools.find_packages(),
     install_requires=["httpx", "beautifulsoup4", "lxml"],
     classifiers=[
