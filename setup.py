2a408c9ea99b0f04ced7aa96f2dcb3d8be106205
AmirSTM <amirstm@gmail.com>
2023-10-01 12:47:21 +0330

    Add github actions for testing,linting, and packaging

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setuptools.setup(
     author_email="info@arkaequities.com",
     description="General utilities used for data mining in Tehran Stock Exchange (TSE).",
     long_description="Utilities for data mining and auto-trading in Tehran Stock Exhchange. \
-        Contains TSETMC API implementation and other interfaces useful for securities market \
-        data catching and processing",
+        Contains TSETMC API implementation and other useful interfaces for securities market \
+        data catching and processing.",
     packages=setuptools.find_packages(),
     install_requires=["httpx", "beautifulsoup4", "lxml"],
     classifiers=[
